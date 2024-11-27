0e2d9844c4ca3fe65f4b169285813ca718d3a243
adebola <adebola.aremu@titantrustbank.com>
2024-11-27 14:02:20 +0100

    first commit

---
+++ src/Infrastructure/SmartCleanArchitecture.Infrastructure/IRepository/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using SmartCleanArchitecture.Domain.Entities.Dals;
+using SmartCleanArchitecture.Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCleanArchitecture.Infrastructure.IRepository
+{
+    public interface IUnitOfWork
+    {
+     
+        IGenericRepository<UsersTest> Users { get; }
+       
+    }
+}
--- src/Presentation/SmartCleanArchitecture.Api/Controllers/UsersController.cs
+++ src/Presentation/SmartCleanArchitecture.Api/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SmartCleanArchitecture.Application.Commands;
+using SmartCleanArchitecture.Infrastructure.IRepository;
+using SmartCleanArchitecture.Infrastructure.Model;
+using SmartCleanArchitecture.Infrastructure.Repository;
 
 namespace SmartCleanArchitecture.Api.Controllers
 {
@@ -8,6 +11,14 @@ namespace SmartCleanArchitecture.Api.Controllers
     [ApiController]
     public class UsersController : ApiControllerBase
     {
+        private readonly IUnitOfWork unitOfWork;
+        IConfiguration _configuration;
+
+        public UsersController(IUnitOfWork unitOfWork, IConfiguration configuration)
+        {
+            this.unitOfWork = unitOfWork;
+            _configuration = configuration;
+        }
         [HttpPost, Route("OnboardSS0User")]
         public async Task<IActionResult> CreateSSOUser([FromBody] CreateUserCommand command)
         {
@@ -15,5 +26,17 @@ namespace SmartCleanArchitecture.Api.Controllers
             return Ok(res);
         }
 
+
+        [HttpGet]
+        public async Task<IEnumerable<UsersTest>> GetBrands()
+        {
+            return await unitOfWork.Users.Get("SELECT * FROM USERSTEST");
+        }
+
+        [HttpGet, Route("FindUser")]
+        public async Task<IEnumerable<UsersTest>> FindBrands(string ID)
+        {
+            return await unitOfWork.Users.Get($"SELECT * FROM USERSTEST where id ={ID}");
+        }
     }
 }
